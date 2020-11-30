81e98c2d46743f016260989802d56258c74b57f5
Tuan <tuan.voanh@hdwebsoft.co>
2020-11-30 16:43:30 +0700

    apply cors *

--- app.js
+++ app.js
@@ -33,6 +33,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 // Routes
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use("/users", userRoute);
 app.use("/shops", shopRoute);
 
