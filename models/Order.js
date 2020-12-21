17136c2af2d6321f811dd5949af46113910b344e
Tuan <tuan.voanh@hdwebsoft.co>
2020-12-21 18:39:12 +0700

    add more index

--- models/Order.js
+++ models/Order.js
@@ -4,8 +4,9 @@ const Schema = mongoose.Schema
 const OrderSchema = new Schema({
     isNoted: {type: Boolean, default: false},
     last_updated: {type: String, index: true},
+    order_time: {type: String, index: true},
     shopId: {type: String, index: true},
 }, {strict: false})
-
+OrderSchema.index({ shopId: 1, state: 1});
 const Order = mongoose.model('Order', OrderSchema)
 module.exports = Order
